d87c96865639d05e790afaeb91e4b39cbcc53e99
Shaganpreet Singh Grewal <grewalshagan94@gmail.com>
2018-04-14 14:31:34 -0500

    New Commit

--- src/decisiontree/DecisionTree.java
+++ src/decisiontree/DecisionTree.java
@@ -8,43 +8,20 @@ import java.util.HashSet;
 public class DecisionTree implements Classifier{
 
     Node treeRoot;
-
     Random random;
-
-    /*
-     * Randomize tree (for random forest)?  If true, chooses some subset of
-     * attributes to choose best from at each node
-     */
     boolean randomize;
 
-    /*
-     * How many features do we want to choose from at each node in randomized
-     * tree?
-     */
     private int numFeatures(int total) {
         return (int)Math.sqrt(total) + 1;
-        //return (int)Math.log(total) + 1;
     }
 
-    /*
-     *  Inner class to represent tree structure - Splits on binary features
-     */
     private class Node {
-        /*
-         * What attribute does this branch split on? -1 indicates nothing
-         */
+        
         public int attribute;
-        /*
-         * What is the label for this node?
-         */
         public int label;
 
         public Node[] children;
 
-        /*
-         * Entropy of boolean variable - n negative examples, p positive
-         * examples
-         */
         double entropy(double n, double p) {
             if (n == 0 || p == 0)
                 return 0.0;
@@ -58,21 +35,18 @@ public class DecisionTree implements Classifier{
             int bestAttr = -1;
             double bestGain = -1;
             int[] labelCount = new int[2];
-            for (int ex : examples) {
-                //Yay array index beauty...
+            for (int ex : examples){
                 labelCount[data.trainLabel[ex]]++;
             }
             double setEntropy = entropy(labelCount[0], labelCount[1]);
             for (int attr : attributes) {
                 double[][] count = new double[data.attrVals[attr].length][2];
                 for (int ex : examples) {
-                    //Yay array index beauty...
                     count[data.trainEx[ex][attr]][data.trainLabel[ex]]++;
                 }
                 
                 double gain = setEntropy;
                 for (int val = 0; val < data.attrVals[attr].length; val++) {
-                    //Get number of examples with this attribute value
                     gain -= ((count[val][0] + count[val][1]) / examples.size())
                                 * entropy(count[val][0], count[val][1]);
                 }
@@ -81,7 +55,6 @@ public class DecisionTree implements Classifier{
                     bestAttr = attr;
                     bestGain = gain;
                 }
-                //System.out.println(gain);
             }
             return bestAttr;
         }
@@ -93,13 +66,10 @@ public class DecisionTree implements Classifier{
 
             if (examples.size() == 0) {
                 this.attribute = -1;
-                this.label = 0; //To avoid crashes, although parent must set
-                return;         //      the label on this node
+                this.label = 0; 
+                return;         
             }
 
-            /*
-             * Calculates majority class
-             */
             int majority = 0;
             int count[] = new int[2];
 
@@ -109,80 +79,47 @@ public class DecisionTree implements Classifier{
 
             majority = (count[1] > count[0] ? 1 : 0);
 
-            /*
-             * They all have the same label or there are no more attributes to
-             * split on
-             */
             if (count[majority] == examples.size() || attributes.size() == 0) {
-                //System.out.println(count[0] + ":" + count[1]);
                 this.attribute = -1;
                 this.label = majority;
                 return;
             }
 
-            /*
-             * If randomization is on (i.e. being used in a random forest), then
-             * we want to choose some random subset of features to choose best
-             * split feature in.
-             */
             if (randomize) {
                 int numAttr = numFeatures(attributes.size());
-                //More efficients ways to do this, but this works well enough
                 HashSet<Integer> attrSample = new HashSet<Integer>(numAttr);
                 for (int attr : attributes) {
-                    /*
-                     * Add each with a probability of numAttr/number attr
-                     * Also adds some variance so we slightly randomize the
-                     * exact number
-                     */
                     if (random.nextInt(attributes.size()) < numAttr) {
                         attrSample.add(attr);
                     }
                 }
-                //System.out.println(numAttr + " : " + attrSample.size());
                 this.attribute = chooseAttribute(data, attrSample, examples);
             } else {
                 this.attribute = chooseAttribute(data, attributes, examples);
             }
 
-            /*No best attribute*/
             if (this.attribute == -1) {
                 this.label = majority;
                 return;
             }
 
-            //Remove the attribute so it cannot be used again in child branches
-            //Add it back in before returning
             attributes.remove(this.attribute);
 
-            //Initialize list of examples that will be passed to children
             ArrayList<ArrayList<Integer>> childExamples = new
                 ArrayList<ArrayList<Integer>>
                         (data.attrVals[this.attribute].length);
             for (int i = 0; i < data.attrVals[this.attribute].length; i++) {
                 childExamples.add(new ArrayList<Integer>());
             }
-            //for (ArrayList<Integer> l : childExamples) {
-                //l = new ArrayList<Integer>();
-            //}
-
-            /*
-             * Split examples based on the chosen attribute
-             */
             for (int ex : examples) {
                 childExamples.get(data.trainEx[ex][this.attribute]).add(ex);
             }
 
             children = new Node[data.attrVals[this.attribute].length];
-            /*Create children trees*/
             for (int i = 0; i < data.attrVals[this.attribute].length; i++) {
                 children[i] = new Node(data,
                                         attributes,
                                         childExamples.get(i));
-                /*
-                 * Need to set child label if they don't have any examples to train
-                 * on
-                 */
                 if (childExamples.get(i).size() == 0) {
                     children[i].label = majority;
                 }
@@ -191,7 +128,6 @@ public class DecisionTree implements Classifier{
         }
     }
 
-    /*Just takes dataset - uses all attributes in training*/
     public DecisionTree(DataSet data, boolean rand) {
         random = new Random();
 
@@ -199,27 +135,23 @@ public class DecisionTree implements Classifier{
         HashSet<Integer> attributes = new HashSet<Integer>(data.numAttrs);
         ArrayList<Integer> examples = new ArrayList<Integer>(data.numTrainExs);
 
-        /*Initialize example and attribute lists*/
         for (int i = 0; i < data.numAttrs; i++) { attributes.add(i); }
         for (int i = 0; i < data.numTrainExs; i++) { examples.add(i); }
 
         treeRoot = new Node(data, attributes, examples);
     }
 
-    /*Takes the dataset and attributes to use in training*/
     public DecisionTree(DataSet data, HashSet<Integer> attributes, boolean rand) {
         random = new Random();
 
         this.randomize = rand; //Randomized tree?
 
-        /*Initialize example lists to include all examples*/
         ArrayList<Integer> examples = new ArrayList<Integer>(data.numTrainExs);
         for (int i = 0; i < data.numTrainExs; i++) { examples.add(i); }
 
         treeRoot = new Node(data, attributes, examples);
     }
 
-    /*Take both attributes and examples to use for training*/
     public DecisionTree(DataSet data, HashSet<Integer> attributes,
             ArrayList<Integer> examples, boolean rand) {
         random = new Random();
@@ -227,40 +159,23 @@ public class DecisionTree implements Classifier{
         treeRoot = new Node(data, attributes, examples);
     }
 
-    /**  
-     * Walks down the generated tree to return a label for the example.
-     */
     public int predict(int[] ex) {
         Node current = treeRoot;
         int depth = 0;
         while (current.attribute != -1) {
             current = current.children[ex[current.attribute]];
         }
-        //System.out.println(depth);
-        //System.out.println(current.attribute + "\t" + current.label);
         return current.label;
     }
 
-    /** This method should return a very brief but understandable
-     * description of the learning algorithm that is being used,
-     * appropriate for posting on the class website.
-     */
     public String algorithmDescription() {
         return "Basic decision tree for use with random forests";
     }
 
-    /** This method should return the "author" of this program as you
-     * would like it to appear on the class website.  You can use your
-     * real name, or a pseudonym, or a name that identifies your
-     * group.
-     */
     public String Author() {
         return "dmrd";
     }
 
-    /*
-     * Simple main for testing.
-     */
     public static void main(String argv[])
         throws FileNotFoundException, IOException {
 
@@ -271,16 +186,8 @@ public class DecisionTree implements Classifier{
 
         String filestem = argv[0];
 
-        /*
-         * Create a cross validation set - just takes the last crossSize
-         * elements of the set as a cross set.
-         */
         DiscreteDataSet d = new DiscreteDataSet(filestem);
 
-        /*
-         * Do the Knuth Shuffle!  It sounds like more fun than it is!
-         */
-        //Set seed to constant to get the same result multiple times
         Random random = new Random();
         for (int i = 0; i < d.numTrainExs; i++) {
             int swap = random.nextInt(d.numTrainExs - i);
@@ -292,7 +199,6 @@ public class DecisionTree implements Classifier{
             d.trainLabel[d.numTrainExs - i - 1] = tempLabel;
         }
 
-        //What proportion of the dataset to use for testing
         int crossSize = d.numTrainExs/8;
 
         int[][] crossEx = new int[crossSize][];
@@ -311,7 +217,6 @@ public class DecisionTree implements Classifier{
             crossLabel[i] = d.trainLabel[d.numTrainExs - i - 1];
         }
 
-        //Modify original dataset
         d.numTrainExs = dEx.length;
         d.trainEx = dEx;
         d.trainLabel = dLabel;
